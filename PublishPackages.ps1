c0fc9466f66e3ef65861f117827bcc1ffb64205e
WolfgangHG <wolfgang.knauf@gmx.de>
2026-02-15 11:53:40 +0100

    Release 1.0.0

--- PublishPackages.ps1
+++ PublishPackages.ps1
@@ -1,14 +1,41 @@
 try{
+  #Move to script directory (when starting from VSCode)
+  Set-Location (Split-Path $script:MyInvocation.MyCommand.Path)
+
+  #Find package version:
+  [Microsoft.PowerShell.Commands.MatchInfo] $PackageVersionMatch = Select-String -Path "Directory.Build.targets"  -Pattern "<PackageVersion>(.*)</PackageVersion>"
+  if ($null -eq $PackageVersionMatch) {
+    #Should not happen.
+    Write-Host -ForegroundColor Red "Could not determine PackageVersion"
+    Read-Host -Prompt "Please press enter"
+    exit
+  }
+  [string] $PackageVersion = $PackageVersionMatch.Matches[0].Groups[1].Value
+
+  Write-Host "Publish version $PackageVersion to: "
+  Write-Host "  1) Nuget integration enviromment (https://int.nugettest.org/)"
+  Write-Host "  2) Nuget (https://www.nuget.org/)"
+  [string] $PublishTargetSelection = Read-Host "Please enter number"
+  [string] $PublishServer = ""
+  if ($PublishTargetSelection -eq "1") {
+    $PublishServer = "https://apiint.nugettest.org/v3/index.json"
+  }
+  elseif ($PublishTargetSelection -eq "2") {
+    $PublishServer = "https://api.nuget.org/v3/index.json"
+  }
+  else {
+    Write-Host -ForegroundColor Red "Invalid selection: $PublishTargetSelection"
+    Read-Host -Prompt "Please press enter"
+    exit
+  }
+
   [string] $ApiKey = Read-Host "Enter API key"
 
-  #TODO: find package version
-  [string] $PackageVersion = "0.1.0-Beta1"
-
-  Start-Process -FilePath "dotnet.exe" -ArgumentList "nuget", "push", "Packages\XbyOpenApi.Core.$PackageVersion.nupkg", "--api-key $apiKey", "--source https://apiint.nugettest.org/v3/index.json" -Wait -NoNewWindow
-  Start-Process -FilePath "dotnet.exe" -ArgumentList "nuget", "push", "Packages\XbyOpenApi.OAuth1.$PackageVersion.nupkg", "--api-key $apiKey", "--source https://apiint.nugettest.org/v3/index.json" -Wait -NoNewWindow
-  Start-Process -FilePath "dotnet.exe" -ArgumentList "nuget", "push", "Packages\XbyOpenApi.OAuth1.WinForms.$PackageVersion.nupkg", "--api-key $apiKey", "--source https://apiint.nugettest.org/v3/index.json" -Wait -NoNewWindow
-  Start-Process -FilePath "dotnet.exe" -ArgumentList "nuget", "push", "Packages\XbyOpenApi.OAuth2.$PackageVersion.nupkg", "--api-key $apiKey", "--source https://apiint.nugettest.org/v3/index.json" -Wait -NoNewWindow
-  Start-Process -FilePath "dotnet.exe" -ArgumentList "nuget", "push", "Packages\XbyOpenApi.OAuth2.WinForms.$PackageVersion.nupkg", "--api-key $apiKey", "--source https://apiint.nugettest.org/v3/index.json" -Wait -NoNewWindow
+  Start-Process -FilePath "dotnet.exe" -ArgumentList "nuget", "push", "Packages\XbyOpenApi.Core.$PackageVersion.nupkg", "--api-key $apiKey", "--source $PublishServer" -Wait -NoNewWindow
+  Start-Process -FilePath "dotnet.exe" -ArgumentList "nuget", "push", "Packages\XbyOpenApi.OAuth1.$PackageVersion.nupkg", "--api-key $apiKey", "--source $PublishServer" -Wait -NoNewWindow
+  Start-Process -FilePath "dotnet.exe" -ArgumentList "nuget", "push", "Packages\XbyOpenApi.OAuth1.WinForms.$PackageVersion.nupkg", "--api-key $apiKey", "--source $PublishServer" -Wait -NoNewWindow
+  Start-Process -FilePath "dotnet.exe" -ArgumentList "nuget", "push", "Packages\XbyOpenApi.OAuth2.$PackageVersion.nupkg", "--api-key $apiKey", "--source $PublishServer" -Wait -NoNewWindow
+  Start-Process -FilePath "dotnet.exe" -ArgumentList "nuget", "push", "Packages\XbyOpenApi.OAuth2.WinForms.$PackageVersion.nupkg", "--api-key $apiKey", "--source $PublishServer" -Wait -NoNewWindow
 
   Read-Host -Prompt "Please press enter"
 }
